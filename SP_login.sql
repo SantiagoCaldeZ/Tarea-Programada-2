6d444d6c21988df89d58ff5d07e58e328616b98e
SantiagoCaldeZ <santiagocalderooon@gmail.com>
2025-09-30 23:30:25 -0600

    Update SP_login.sql

--- SP_login.sql
+++ SP_login.sql
@@ -1,5 +1,5 @@
 CREATE OR ALTER PROCEDURE [dbo].[sp_Login]
-    @inUsuario NVARCHAR(64),
+    @inUsuario NVARCHAR(50),
     @inPassword NVARCHAR(64),
     @outCodigo INT OUTPUT
 AS
@@ -7,30 +7,24 @@ BEGIN
     SET NOCOUNT ON;
 
     BEGIN TRY
-        -- Verificar si existe
+        -- Verificar existencia
         IF NOT EXISTS (SELECT 1 FROM dbo.Usuario WHERE Username = @inUsuario)
         BEGIN
             SET @outCodigo = 50001; -- Usuario no existe
-            RETURN @outCodigo;
+            RETURN;
         END
 
         -- Validar credenciales
-        IF NOT EXISTS (
-            SELECT 1 FROM dbo.Usuario 
-            WHERE Username = @inUsuario AND Password = @inPassword
-        )
+        IF NOT EXISTS (SELECT 1 FROM dbo.Usuario WHERE Username = @inUsuario AND Password = @inPassword)
         BEGIN
-            SET @outCodigo = 50002; -- Contraseña incorrecta
-            RETURN @outCodigo;
+            SET @outCodigo = 50002; -- ContraseÃ±a incorrecta
+            RETURN;
         END
 
         -- Login exitoso
         SET @outCodigo = 0;
-        RETURN @outCodigo;
     END TRY
     BEGIN CATCH
         SET @outCodigo = 50099; -- Error inesperado
-        RETURN @outCodigo;
     END CATCH
 END
-GO
