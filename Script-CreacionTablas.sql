9ce31cb9b1e0a2e44253ed08a69cc793907d0411
Andres-2908 <andresvalverdeg@gmail.com>
2025-09-30 22:38:13 -0600

    Script-CreacionTablas

---
+++ Script-CreacionTablas.sql
@@ -0,0 +1,181 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[BitacoraEvento](
+	[Id] [int] NOT NULL,
+	[IdTipoEvento] [int] NOT NULL,
+	[Descripcion] [varchar](64) NOT NULL,
+	[IdPostByUser] [int] NOT NULL,
+	[PostInIP] [varchar](32) NOT NULL,
+	[PostTime] [datetime] NOT NULL,
+ CONSTRAINT [PK_BitacoraEvento] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[DBError](
+	[Id] [int] NOT NULL,
+	[UserName] [varchar](64) NOT NULL,
+	[Number] [int] NOT NULL,
+	[State] [varchar](64) NOT NULL,
+	[Severity] [varchar](64) NOT NULL,
+	[Line] [varchar](64) NOT NULL,
+	[Procedure] [varchar](64) NOT NULL,
+	[Message] [varchar](64) NOT NULL,
+	[DateTime] [datetime] NOT NULL,
+ CONSTRAINT [PK_DBError] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Empleado](
+	[Id] [int] NOT NULL,
+	[IdPuesto] [int] NOT NULL,
+	[ValorDocumentoIdentidad] [varchar](64) NOT NULL,
+	[Nombre] [varchar](64) NOT NULL,
+	[FechaContratacion] [date] NOT NULL,
+	[SaldoVacaciones] [money] NOT NULL,
+	[EsActivo] [bit] NOT NULL,
+ CONSTRAINT [PK_Empleado] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Error](
+	[Id] [int] NOT NULL,
+	[Codigo] [int] NOT NULL,
+	[Descripcion] [varchar](64) NOT NULL,
+ CONSTRAINT [PK_Error] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Movimiento](
+	[Id] [int] NOT NULL,
+	[IdEmpleado] [int] NOT NULL,
+	[IdTipoMovimiento] [int] NOT NULL,
+	[Fecha] [date] NOT NULL,
+	[Monto] [money] NOT NULL,
+	[NuevoSaldo] [money] NOT NULL,
+	[IdPostByUser] [int] NOT NULL,
+	[PostInIP] [varchar](32) NOT NULL,
+	[PostTime] [datetime] NOT NULL,
+ CONSTRAINT [PK_Movimiento] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Puesto](
+	[Id] [int] NOT NULL,
+	[Nombre] [varchar](64) NOT NULL,
+	[SalarioxHora] [money] NOT NULL,
+ CONSTRAINT [PK_Puesto] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[TipoEvento](
+	[Id] [int] NOT NULL,
+	[Nombre] [varchar](64) NOT NULL,
+ CONSTRAINT [PK_TipoEvento] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[TipoMovimiento](
+	[Id] [int] NOT NULL,
+	[Nombre] [varchar](64) NOT NULL,
+	[TipoAccion] [varchar](32) NOT NULL,
+ CONSTRAINT [PK_TipoMovimiento] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Usuario](
+	[Id] [int] NOT NULL,
+	[Username] [varchar](64) NOT NULL,
+	[Password] [varchar](64) NOT NULL,
+ CONSTRAINT [PK_Usuario] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[BitacoraEvento]  WITH CHECK ADD  CONSTRAINT [FK_BitacoraEvento_TipoEvento] FOREIGN KEY([IdTipoEvento])
+REFERENCES [dbo].[TipoEvento] ([Id])
+GO
+ALTER TABLE [dbo].[BitacoraEvento] CHECK CONSTRAINT [FK_BitacoraEvento_TipoEvento]
+GO
+ALTER TABLE [dbo].[BitacoraEvento]  WITH CHECK ADD  CONSTRAINT [FK_BitacoraEvento_Usuario] FOREIGN KEY([IdPostByUser])
+REFERENCES [dbo].[Usuario] ([Id])
+GO
+ALTER TABLE [dbo].[BitacoraEvento] CHECK CONSTRAINT [FK_BitacoraEvento_Usuario]
+GO
+ALTER TABLE [dbo].[Empleado]  WITH CHECK ADD  CONSTRAINT [FK_Empleado_Puesto] FOREIGN KEY([IdPuesto])
+REFERENCES [dbo].[Puesto] ([Id])
+GO
+ALTER TABLE [dbo].[Empleado] CHECK CONSTRAINT [FK_Empleado_Puesto]
+GO
+ALTER TABLE [dbo].[Movimiento]  WITH CHECK ADD  CONSTRAINT [FK_Movimiento_Empleado] FOREIGN KEY([IdEmpleado])
+REFERENCES [dbo].[Empleado] ([Id])
+GO
+ALTER TABLE [dbo].[Movimiento] CHECK CONSTRAINT [FK_Movimiento_Empleado]
+GO
+ALTER TABLE [dbo].[Movimiento]  WITH CHECK ADD  CONSTRAINT [FK_Movimiento_TipoMovimiento] FOREIGN KEY([IdTipoMovimiento])
+REFERENCES [dbo].[TipoMovimiento] ([Id])
+GO
+ALTER TABLE [dbo].[Movimiento] CHECK CONSTRAINT [FK_Movimiento_TipoMovimiento]
+GO
+ALTER TABLE [dbo].[Movimiento]  WITH CHECK ADD  CONSTRAINT [FK_Movimiento_Usuario] FOREIGN KEY([IdPostByUser])
+REFERENCES [dbo].[Usuario] ([Id])
+GO
+ALTER TABLE [dbo].[Movimiento] CHECK CONSTRAINT [FK_Movimiento_Usuario]
+GO
