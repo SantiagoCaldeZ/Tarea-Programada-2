a5ff3882d4f54a8f3cd44370d8e7bd5b3647ca14
Andres-2908 <andresvalverdeg@gmail.com>
2025-10-19 22:03:25 -0600

    Add files via upload

---
+++ sp_ObtenerTiposMovimiento.sql
@@ -0,0 +1,23 @@
+﻿CREATE OR ALTER PROCEDURE sp_ObtenerTiposMovimiento
+    @outCodigo INT OUTPUT
+AS
+BEGIN
+    SET NOCOUNT ON;
+    BEGIN TRY
+        IF EXISTS (SELECT 1 FROM TipoMovimiento)
+        BEGIN
+            SELECT Id, Nombre, TipoAccion
+            FROM TipoMovimiento
+            ORDER BY Nombre;
+            SET @outCodigo = 0;
+        END
+        ELSE
+        BEGIN
+            SET @outCodigo = 50040; -- sin registros
+        END
+    END TRY
+    BEGIN CATCH
+        SET @outCodigo = 50041; -- error inesperado
+    END CATCH
+END;
+GO
