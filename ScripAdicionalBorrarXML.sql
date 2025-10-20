181f3f16f41606c16a626792e4b9acf6fdab6db5
Andres-2908 <andresvalverdeg@gmail.com>
2025-10-20 00:24:38 -0600

    Script adicional para el XML
    Este script simplemente hace un select de todas las tablas que son modificadas por el script del XML ademas de un delete de las tablas y un Reseed para poder volver a ejecutar el script del XML para demostrar su funcionamiento.

---
+++ ScripAdicionalBorrarXML.sql
@@ -0,0 +1,34 @@
+select * from dbo.Puesto
+select * from dbo.TipoEvento
+select * from dbo.TipoMovimiento
+select * from dbo.Usuario
+select * from dbo.Error
+select * from dbo.Empleado
+select * from dbo.Movimiento
+
+delete from dbo.Movimiento --7
+delete from dbo.Empleado --6
+delete from dbo.Puesto --1
+delete from dbo.TipoEvento --2
+delete from dbo.TipoMovimiento --3
+delete from dbo.Usuario --4
+delete from dbo.Error --5
+
+DBCC CHECKIDENT ('dbo.Puesto', RESEED, 0);
+DBCC CHECKIDENT ('dbo.TipoEvento', RESEED, 0);
+DBCC CHECKIDENT ('dbo.TipoMovimiento', RESEED, 0);
+DBCC CHECKIDENT ('dbo.Usuario', RESEED, 0);
+DBCC CHECKIDENT ('dbo.Error', RESEED, 0);
+DBCC CHECKIDENT ('dbo.Empleado', RESEED, 0);
+DBCC CHECKIDENT ('dbo.Movimiento', RESEED, 0);
+
+
+
+select * from dbo.BitacoraEvento
+select * from DBError
+
+
+
+
+
+
